2bcb09346e4b16ae45520ee22b1f31495ecee3f7
Blake Wallick <bwallick@hawaii.edu>
2017-11-15 11:00:11 -1000

    Incremental Changes to Profile. Collections references now working. But not displaying.

--- app/imports/startup/server/initialize-database.js
+++ app/imports/startup/server/initialize-database.js
@@ -30,7 +30,7 @@ function restoreCollection(collection, restoreJSON) {
 
 Meteor.startup(() => {
   /** Only initialize database if it's empty. */
-  const collectionList = [Interests, Profiles, Games];
+  const collectionList = [Interests, Games, Profiles];
   const totalDocuments = _.reduce(collectionList, function reducer(memo, collection) {
     return memo + collection.count();
   }, 0);
